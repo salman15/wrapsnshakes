4bf5af895d4dc15c6b5d60fc6c72c55eced2d6bd
salman <salmannova@gmail.com>
2020-02-27 08:06:37 +0100

    build added

---
+++ build/precache-manifest.22d46cf0864f415183d72310c8e4d991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb050b31a1f75378a40fd5d6ca6c60c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a798954490d0376188b7",
+    "url": "/static/css/main.5c8b7474.chunk.css"
+  },
+  {
+    "revision": "1d9d5edc0c6feeb78b01",
+    "url": "/static/js/2.464ea7bd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.464ea7bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a798954490d0376188b7",
+    "url": "/static/js/main.09a90773.chunk.js"
+  },
+  {
+    "revision": "cef109f4da7139b94312",
+    "url": "/static/js/runtime-main.7cbdded0.js"
+  },
+  {
+    "revision": "7655d129d0addb2f08a5c1dc994aaa4b",
+    "url": "/static/media/brush.7655d129.ttf"
+  }
+]);
